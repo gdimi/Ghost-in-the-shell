8a43a45dec7a64fc57890405a0bde5f4540bcb96
Zsombor Paroczi <zsombor.paroczi@gmail.com>
2016-08-03 00:26:13 +0200

    assert callback

--- patterns/preg.php
+++ patterns/preg.php
@@ -3,6 +3,7 @@ $patternPreg = array(
     '/\$GLOBALS\[(.*)\];global\$(.*)exit\(\)\;}/i' => 'some $GLOBALS virus',
     '/\$GLOBALS\[(.*)\]\((.*)\)/i' => 'call to $GLOBALS[...](...)',
     '/sprintf\(\$([0-9a-zA-Z]*)\(/i'=>'$O00OO0 virus',
-    '/\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)[ ]*\(/i' => 'call variable function'
+    '/\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)[ ]*\(/i' => 'call variable function',
+    '/\@assert\(/i'=>'assert callback',
 );
 ?>
